a3fb6dd6b1d25f47ef6b9c86a7951aa856b23141
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-08-11 13:35:54 -0700

    made login button functional and chained methods in app.js

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ var g = G$('John', 'Doe')
 //the first function returns 'this', making it chainable. Pass in 'true' to get formal greeting
 //now i have chainable methods and an object I created without having to to call new, using the jQuery syntax
 g.greet().setLang('es').greet(true).log()
+
+
+$('#login').click(function(){
+
+  var loginGrtr = G$('John', 'Doe')
+
+  $('#logindiv').hide()
+
+  loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log()
+})
